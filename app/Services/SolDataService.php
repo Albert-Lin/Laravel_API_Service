563e3f1ceb1223ae2b7df6074ccf0c1a575125d6
Albert-Lin <familywithloveg@gmail.com>
2018-02-07 18:13:29 +0800

    01. 開始建立 URI format 處理

--- app/Services/SolDataService.php
+++ app/Services/SolDataService.php
@@ -28,6 +28,7 @@ class SolDataService
 		// 03_3. add base url
 //		$htmlStr = str_replace("<head>", "<head> <base href='http://www.cna.com.tw'>", $htmlStr);
 		
+		$this->absoluteURI($htmlStr, $url);
 		
 		return $this->toUTF8($htmlStr);
 	}
@@ -39,5 +40,22 @@ class SolDataService
 		$encode = preg_replace("/\"/", "", $encode);
 		$encode = preg_replace("/\//", "", $encode);
 		return mb_convert_encoding($htmlStr, "UTF-8", $encode);
+	}
+	
+	public function absoluteURI ($htmlStr, $url) {
+		// 01. change $url to an stdClass
+		$schemeSplit = preg_split("/:\/\//", $url);
+		$allPathSplit = preg_split("/\//", $schemeSplit[1]);
+		$authoritySplit = preg_split("/@/", $allPathSplit[0]);
+		$pageSplit = preg_split("/\?/", $allPathSplit[count($allPathSplit)-1]);
+		$uri = new \stdClass();
+		$uri->scheme = $schemeSplit[0];
+		$uri->userInfo = (count($authoritySplit) > 1) ? $authoritySplit[0] : null;
+		$uri->hostname = (count($authoritySplit) > 1) ? $authoritySplit[1] : $authoritySplit[0];
+		$uri->path = array_slice($allPathSplit, 1, count($allPathSplit)-1);
+		$uri->page = (count($pageSplit) > 1) ? $pageSplit[0] : null;
+		$uri->query = (count($pageSplit) > 1) ? $pageSplit[1] : $pageSplit[0];
+		
+		var_dump($uri) & die;
 	}
 }
